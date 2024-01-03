1fbfb15835e878bcae40ca88cbf1ff1464286f56
banchet18 <banjarac6@gmail.com>
2024-01-03 11:56:17 +0530

    c

--- projects/src/components/accordion/index.jsx
+++ projects/src/components/accordion/index.jsx
@@ -7,13 +7,20 @@ import { useState } from "react";
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
 
+  const handleSingleSelection = (getCurrentId) => {
+    console.log(getCurrentId);
+  };
+
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div className="title">
+              <div
+                className="title"
+                onClick={() => handleSingleSelection(dataItem.id)}
+              >
                 <h1>{dataItem.question}</h1>
                 <span>+</span>
               </div>
