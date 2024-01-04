08d384d1ea0771a644f1362897125670450e598a
banchet18 <banjarac6@gmail.com>
2024-01-04 11:22:02 +0530

    c

--- cartproject/src/components/Cards.js
+++ cartproject/src/components/Cards.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import CardsData from "./CardsData";
 
 const Cards = () => {
+  const [data, setData] = useState(CardsData);
+
   return (
     <div className="container mt-3">
-      <h2>Add to Cart Projects</h2>
-      <div className="row">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+      <h2 className="text-center">Add to Cart Projects</h2>
+      <div className="row d-flex justify-content-center align-items-center">
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card
+                style={{ width: "22rem", border: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <Card.Img
+                  variant="top"
+                  src={element.imgdata}
+                  style={{ height: "16rem" }}
+                  className="mt-3"
+                />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <div className="button_div d-flex justify-content-center">
+                    <Button variant="primary" className="col-lg-12">
+                      Go somewhere
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
       </div>
     </div>
   );
