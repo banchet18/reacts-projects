f0f0518b550887e446ed447f14bd527fe5006445
banchet18 <banjarac6@gmail.com>
2024-01-04 16:29:44 +0530

    c

--- cartpractice/src/components/Header.js
+++ cartpractice/src/components/Header.js
@@ -1,4 +1,16 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav>
+      </Navbar>
+    </>
+  );
 };
 export default Header;
