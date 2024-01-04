4fbcba9b7b9cf2f7db6cc8be9de49cea533c8eeb
banchet18 <banjarac6@gmail.com>
2024-01-04 08:38:55 +0530

    c

--- cartproject/src/components/Header.js
+++ cartproject/src/components/Header.js
@@ -6,13 +6,11 @@ import Navbar from "react-bootstrap/Navbar";
 const Header = () => {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
