621439fcb0e175f29ee528ca2b7263779b8cf916
banchet18 <banjarac6@gmail.com>
2024-01-04 08:58:09 +0530

    c

--- cartproject/src/App.js
+++ cartproject/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
