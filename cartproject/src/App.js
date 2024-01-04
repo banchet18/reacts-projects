86d41ea3e80ed48bbae401b3fee91645725a8c74
banchet18 <banjarac6@gmail.com>
2024-01-04 09:04:38 +0530

    c

--- cartproject/src/App.js
+++ cartproject/src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
+import Cards from "./components/Cards";
+import CardsDetails from "./components/CardsDetails";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/cart" element={<CardsDetails />} />
+      </Routes>
     </>
   );
 }
