b3d5958266540c6433b5d805cffac52e1c8e80e5
banchet18 <banjarac6@gmail.com>
2024-01-05 08:26:33 +0530

    c

--- cartproject/src/redux/reducers/reducer.js
+++ cartproject/src/redux/reducers/reducer.js
@@ -5,7 +5,7 @@ const INIT_STATE = {
 export const cardReducer = (state = INIT_STATE, action) => {
   switch (action.type) {
     case "ADD_CART":
-      return { ...state, carts: [...state.type, action.payload] };
+      return { ...state, carts: [...state.carts, action.payload] };
     default:
       return state;
   }
