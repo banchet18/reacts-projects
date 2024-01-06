468db07bfe014d254052d87ef0228c769d939ff8
banchet18 <banjarac6@gmail.com>
2024-01-06 08:30:12 +0530

    c

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element /> {/* Routing the componets or pages */}
+        <Route path="/" element /> {/* Routing the Home componets or pages */}
         <Route path="cart" element />
+        {/* Routing the Home componets or pages */}
       </Routes>
     </>
   );
