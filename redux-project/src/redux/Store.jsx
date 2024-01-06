079543f3117059ce8439b6e390fee8294d840c8a
banchet18 <banjarac6@gmail.com>
2024-01-06 15:43:47 +0530

    c

--- redux-project/src/redux/Store.jsx
+++ redux-project/src/redux/Store.jsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"; //importing configStore function from reduxtoolkit
 
 import cartReducer from "./Cartslice"; //importing cartReducer from file
+import ProductSlice from "./ProductSlice";
 
 // store is like storage
 //whenever cart will been called then cartReducer perform the function
-const store = configureStore({ reducer: { cart: cartReducer } });
+const store = configureStore({
+  // multiples states
+  reducer: { cart: cartReducer, product: ProductSlice },
+});
 
 export default store;
