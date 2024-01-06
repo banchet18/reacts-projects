8185046bb0231176dbe098ac4026a2357b0de952
banchet18 <banjarac6@gmail.com>
2024-01-06 08:40:55 +0530

    c

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom"; //importing router
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* Routing the Home componets or pages */}
