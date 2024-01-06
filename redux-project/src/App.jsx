e5e7edb4ff0922ec6e4232d8cb084c4398089126
banchet18 <banjarac6@gmail.com>
2024-01-06 08:35:25 +0530

    c

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom"; //importing router
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element /> {/* Routing the Home componets or pages */}
-        <Route path="cart" element />
+        <Route path="/" element={<Home />} />
+        {/* Routing the Home componets or pages */}
+        <Route path="/cart" element={<Cart />} />
         {/* Routing the Home componets or pages */}
       </Routes>
     </>
